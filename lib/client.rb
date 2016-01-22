bdc065d70310bc8c0b15707b699442a8e72d7130
Riley <rstarnes9@gmail.com>
2016-01-22 10:41:16 -0800

    add Client class with identical methods Stylist

--- lib/client.rb
+++ lib/client.rb
@@ -1,2 +1,52 @@
 class Client
+  attr_reader(:id, :name, :stylist_id)
+
+  def initialize(attributes)
+    @id = attributes.fetch(:id)
+    @name = attributes.fetch(:name)
+    @stylist_id = attributes.fetch(:stylist_id)
+  end
+
+  def self.all
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    clients = []
+    returned_clients.each do |client|
+      name = client.fetch("name")
+      id = client.fetch("id").to_i
+      stylist_id = client.fetch("stylist_id").to_i
+      clients << Client.new({name: name, id: id, stylist_id: stylist_id})
+    end
+    clients
+  end
+
+  def save
+    result = DB.exec("INSERT INTO clients (name, stylist_id) VALUES ('#{name}', '#{stylist_id}') RETURNING id")
+    @id = result.first.fetch("id").to_i
+  end
+
+  def self.find(id)
+    found_client = nil
+    Client.all.each do |client|
+      if client.id == id
+        found_client = client
+      end
+    end
+    found_client
+  end
+
+  def update(name)
+    @name = name.fetch(:name)
+    @id = self.id
+    DB.exec("UPDATE clients SET (name) = ('#{@name}') WHERE id = '#{@id}';")
+  end
+
+  def delete
+    DB.exec("DELETE FROM clients WHERE id = '#{self.id}';")
+  end
+
+  def ==(another_client)
+    self.name == (another_client.name) &&
+    (self.id == (another_client.id)) &&
+    (self.stylist_id == (another_client.stylist_id))
+  end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,1 +1,71 @@
 require('spec_helper')
+
+describe(Client) do
+  describe('#id, #name') do
+    it('returns information when entered into class') do
+      test_client = Client.new({id: nil, name: "Dominique Diaz", stylist_id: 1})
+      expect(test_client.id).to(eq(nil))
+      expect(test_client.name).to(eq("Dominique Diaz"))
+    end
+  end
+
+  describe('.all') do
+    it('is empty at first') do
+      expect(Client.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves input and pushes to an array') do
+      test_client = Client.new({id: nil, name: "Dominique Diaz", stylist_id: 1})
+      test_client.save
+      expect(Client.all).to(eq([test_client]))
+    end
+  end
+
+  describe('#id') do
+    it('assigns an id to a client') do
+      test_client = Client.new({id: nil, name: "Dominique Diaz", stylist_id: 1})
+      test_client.save
+      expect(test_client.id).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#find') do
+    it('finds an entry based on its id number') do
+      test_client = Client.new({id: nil, name: "Dominique Diaz", stylist_id: 1})
+      test_client.save
+      test_client2 = Client.new({id: nil, name: "Frank Furt", stylist_id: 2})
+      test_client2.save
+      expect(Client.find(test_client2.id())).to(eq(test_client2))
+    end
+  end
+
+  describe('.update') do
+    it('updates user information') do
+      test_client = Client.new({id: nil, name: "Dominique Diaz", stylist_id: 1})
+      test_client.save
+      test_client.update({name: "Fred"})
+      expect(test_client.name).to(eq("Fred"))
+    end
+  end
+
+  describe('.delete') do
+    it('deletes a client') do
+      test_client = Client.new({id: nil, name: "Dominique Diaz", stylist_id: 1})
+      test_client.save
+      test_client2 = Client.new({id: nil, name: "Frank Furt", stylist_id: 2})
+      test_client2.save
+      test_client.delete
+      expect(Client.all).to(eq([test_client2]))
+    end
+  end
+
+  describe('#==') do
+    it('is the same client if information is the same') do
+      test_client = Client.new({id: nil, name: "Dominique Diaz", stylist_id: 1})
+      test_client2 = Client.new({id: nil, name: "Dominique Diaz", stylist_id: 1})
+      expect(test_client).to(eq(test_client2))
+    end
+  end
+end
